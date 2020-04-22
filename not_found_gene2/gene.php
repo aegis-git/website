82333ec359a9748e91e5c93078ad88caeed628ed
aegis-git <50698109+aegis-git@users.noreply.github.com>
2020-04-22 21:20:52 +0900

    sqlTest3

--- not_found_gene2/gene.php
+++ not_found_gene2/gene.php
@@ -12,11 +12,11 @@
 
     function getTextSet(){
         $link =mysqli_connect('localhost','yuto','Yuto1218','mydb');
-        $query="select *.* from not_found_table;";
+        $query="select * from not_found_table;";
         if(mysqli_query($link,$query)){
             echo "good";
         }else{
-            echo mysqli_query($link,$query);
+            echo mysqli_query($link,$query)." is q";
         }
     }
 ?>
