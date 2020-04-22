62a2d1106965aa7d6d1779d17e8e874ac080767b
aegis-git <50698109+aegis-git@users.noreply.github.com>
2020-04-22 17:56:36 +0900

    rename

--- not_found_gene2/gene.php
+++ not_found_gene2/gene.php
@@ -1,9 +1,20 @@
 <?php
-    $src="";
-    $src.="<html><head>";
-    $src.="<title>Generator</title>";
-    $src.="</head><body>";
-    $src.="Hello world";
-    $src.="</body></html>";
-    echo $src;
+    echo make_html();
+
+    function make_html(){
+        $src="";
+        $src.="<html>";
+
+        $src.="</html>";
+        return $src;
+    }
+
+    function getTextSet(){
+        $link =mysqli_connect('localhost','yuto','Yuto1218','mydb');
+        if(mysqli_connect_errno()){
+            die("データベースに接続できません:" . mysqli_connect_error() . "\n");
+        }else{
+            echo "データベースの接続に成功しました。\n";
+        }
+    }
 ?>
